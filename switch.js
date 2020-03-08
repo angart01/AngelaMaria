d7badaedc8042c8bb63d989ad3d733a5bf03ba2d
angart01 <48025723+angart01@users.noreply.github.com>
2020-03-08 02:01:05 -0500

    Switch

---
+++ switch.js
@@ -0,0 +1,27 @@
+'use strict'
+// La declaración switch evalúa una expresión,  y compara con (case)
+// ??? DISTINGUIR MAYUSCULAS toUpperCase()/ MINUSCULAS toLowerCase(); - 
+
+var mes = prompt("Ingrese el mes actual en mayúsculas: ", 0);
+var imprime ="";
+
+switch (mes){
+    case "JUNIO":
+        imprime = "Es verano";
+        break;
+    case "DICIEMBRE":
+        imprime = "Es invierno";
+        break;
+    case "MAYO":
+        imprime = "Es Primavera";
+        break;
+    case "MARZO":
+        imprime = "Es otoño";
+        break;
+        //cuando no sucede nada de lo anterior DEFAULT:
+    default:
+        imprime = "No hay comentarios";
+        break;
+    }
+    
+    alert(imprime);
